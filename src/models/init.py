785c71a7dbb567c2e153845d533f621eea4f6004
Slon-Molodets1st <77678666+Slon-Molodets1st@users.noreply.github.com>
2026-08-26 20:13:38 +0300

    Create init.py

---
+++ src/models/init.py
@@ -0,0 +1,9 @@
+"""Pydantic модели для структурированных данных"""
+
+from src.models.contract_schema import ContractSchema
+from src.models.contradiction import ContradictionResult
+
+__all__ = [
+    'ContractSchema',
+    'ContradictionResult'
+]
